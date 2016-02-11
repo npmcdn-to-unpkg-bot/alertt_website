d745487b0248cff4e4b094aa031559abacf7de3c
Beshad Ghorbani <admin@xeba.co.nz>
2016-02-12 10:26:42 +1300

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(function() {
+    $('#fullpage').fullpage();
     var BV = new $.BigVideo({container: $('#page')});
     BV.init();
     BV.show('video/alertt.mp4',{ambient:true});
 
-    $('#next-btn').on('click', function(e) {
-        $('#page').addClass('animated slideOutUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-            $('#next-btn').css('display','none');
-            $('#body').css('display','block');
-        });
-    });
+    //$('#next-btn').on('click', function(e) {
+    //    $('#page').addClass('animated slideOutUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    //        $('#next-btn').css('display','none');
+    //        $('#body').css('display','block');
+    //    });
+    //});
 
     setInterval(function(){
         $('#next-btn').addClass('animated bounce').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
@@ -19,6 +20,10 @@ $(function() {
     $("#submit-button").click(function() {
         var email = $("#email").val();
         $.post('http://localhost:63342/alertt_website/emails.php', {email: email});
+    });
+
+    $('#next-btn').click(function(){
+        $.fn.fullpage.moveSectionDown();
     });
 
 });
